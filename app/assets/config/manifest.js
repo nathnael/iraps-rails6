4f25ca6a368fbf98d82fdcd41d22f01c427c6b76
Nathnael Getahun <contact.nathnael@gmail.com>
2020-12-05 00:23:30 +0300

    Fix stylesheet import

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,3 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_directory ../datatables .css
-//= link_directory ../javascripts .js
+//= link_tree ../stylesheets .css
+//= link_tree ../javascripts .js
